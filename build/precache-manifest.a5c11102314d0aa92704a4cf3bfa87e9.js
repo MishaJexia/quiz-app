2059a4acbb8724866971dac37790bd05a26a0a87
Lorenzo <lorenzozar@gmail.com>
2020-07-14 11:56:20 +0300

    build

---
+++ build/precache-manifest.a5c11102314d0aa92704a4cf3bfa87e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c66ffe6cad0a8d8a7c26701aa409fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "345c87b992cabfc0302e",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "0637a773460fc5c2a18f",
+    "url": "/static/js/2.669d0908.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.669d0908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345c87b992cabfc0302e",
+    "url": "/static/js/main.a203fd87.chunk.js"
+  },
+  {
+    "revision": "c56dfb8a3664b0dbc42f",
+    "url": "/static/js/runtime-main.845301c6.js"
+  }
+]);
